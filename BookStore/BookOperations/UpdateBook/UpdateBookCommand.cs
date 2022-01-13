48837b406b0106c46faab3c3baa0ca6b0275454d
ipek <ipek_serttas99@htomail.com>
2022-01-13 17:55:01 +0300

    düzeltmeler

--- BookStore/BookOperations/UpdateBook/UpdateBookCommand.cs
+++ BookStore/BookOperations/UpdateBook/UpdateBookCommand.cs
@@ -32,8 +32,6 @@ namespace BookStore.BookOperations.UpdateBook
         public class UpdateBookModel
         {
             public string Title { get; set; }
-            public int PageCount { get; set; }
-            public DateTime PublishDate { get; set; }
             public int GenreId { get; set; }
         }
     }
